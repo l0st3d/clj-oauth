4be3f2ee23601d949ce9be8c2ce20bf5241d262a
Dan Dukeson <dandukeson@gmail.com>
2011-12-07 12:53:50 +0000

    normalize the url before creating the base string according to RFC5849

--- test/oauth/signature_test.clj
+++ test/oauth/signature_test.clj
@@ -263,3 +263,16 @@
   (is (= (sig/url-form-encode {:hello "there"}) "hello=there"))
   (is (= (sig/url-form-encode {:hello "there" :name "Bill Smith" }) "hello=there&name=Bill%20Smith")))
 
+(deftest
+    #^{:doc "normalizing the URL"}
+  normalize-url
+  (let [normalized "http://example.com/resource"]
+    (is (= normalized (sig/normalize "http://example.com/resource")))
+    (is (= normalized (sig/normalize "HTTP://Example.com/resource")))
+    (is (= normalized (sig/normalize "http://example.com:80/resource")))
+    (is (= normalized (sig/normalize "http://example.com/resource?query=foo&bar=baz")))
+    (is (= normalized (sig/normalize "http://example.com:80/resource?query=foo&bar=baz")))
+    (is (= normalized (sig/normalize "http://example.com:80/resource#fragment")))
+    (is (= "http://example.com:99/resource" (sig/normalize "http://example.com:99/resource")))
+    (is (= "https://example.com/resource" (sig/normalize "https://example.com:443/resource")))
+    (is (= "https://example.com:99/resource" (sig/normalize "https://example.com:99/resource")))))
